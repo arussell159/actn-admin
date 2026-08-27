b3b3b206ee1e95ccfdab4dd58e4bad717b27c9c5
alexrussell159 <alexrussell159@gmail.com>
2026-08-27 15:16:37 -0500

    scroll behind dynamic island attempt 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  viewportFit: "cover",
   colorScheme: "light",
   themeColor: "#ffffff",
 }
